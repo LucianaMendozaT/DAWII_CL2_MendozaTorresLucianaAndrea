ce144b2ed2c14d22c5b02576152f1c46d1c3f668
LucianaMendozaT <lucianamend0124@gmail.com>
2023-10-28 19:45:08 -0500

    primer commit :v

---
+++ src/main/java/pe/edu/cibertec/DAWII_CL2_MendozaTorresLucianaAndrea/DawiiCl2MendozaTorresLucianaAndreaApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.DAWII_CL2_MendozaTorresLucianaAndrea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DawiiCl2MendozaTorresLucianaAndreaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DawiiCl2MendozaTorresLucianaAndreaApplication.class, args);
+	}
+
+}
